d86715482395847468663f959e93b94fd48bc1f5
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2022-02-25 20:30:35 -0300

    req2: completo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    student_number = 0
+
+    for index in permanence_period:
+        try:
+            if index[0] <= target_time and target_time <= index[1]:
+                student_number += 1
+        except (TypeError, ValueError):
+            return None
+
+    return student_number
+
+# print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 5))
